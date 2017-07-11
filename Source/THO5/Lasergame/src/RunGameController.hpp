211cb3f9783a13b11b8ac91b5c919bb34a508e61
Ferdi Stoeltie <ferdistoeltie@gmail.com>
2017-07-11 10:35:35 +0200

    Added doxygen for the RunGameController.hpp

--- Source/THO5/Lasergame/src/RunGameController.hpp
+++ Source/THO5/Lasergame/src/RunGameController.hpp
@@ -1,3 +1,12 @@
+///	\file RunGameController.hpp
+/// The RunGameController file,
+/// contains the RunGameController class declaration only. 
+/// Date file created:
+/// \date 07-07-2017
+/// Date Last Update:
+/// \date 11-07-2017
+
+
 #ifndef RUNGAME_HPP
 #define RUNGAME_HPP
 
@@ -11,25 +20,44 @@ class OLEDBoundary;
 
 /// \Author Ferdi Stoeltie
 /// \brief Controller for the runnable game logic
+/// \date 11-07-2017
 class RunGameController : public rtos::task<>, public KeypadListener {
 private:
-
+	// A reference to the keypad controller. This is required to register itself as a listener
    KeypadController& kpC;
+   
+   // A reference to a sound interface. ISound.setSound is required to set an active sound.
    ISound& sound;
    
+   // The OLEDBoundary hardware interface object.
    OLEDBoundary& oledBoundary;
+   
+   // RTOS
    rtos::flag registerFlag;
    rtos::clock gameTimeSecondsClock;
+   
+   // Primitive data types
    int startOfGameTimestamp;
    int gameDurationMin;
 
 public:
 	
-   
+   /// \author Peter Bonnema 
+   /// \author Marianna Delmaar
+   /// \author Ferdi Stoeltie
+   /// \brief Constructor that needs to be called to initialize the \c RunGameController \c instance.
+   /// \param[in] kpC A reference tot the KeypadController.
+   /// \param[in] sound A handle to the ISound interface.
+   /// \param[in] oledBoundary a reference to the oledBoundary object.
+   /// \param Priority of this rtos::task.
    RunGameController(KeypadController& kpC, ISound& sound, OLEDBoundary& oledBoundary, unsigned int priority );
    ~RunGameController();
 
    void main() override;
+   
+   /// \author Ferdi Stoeltie
+   /// \brief Method that is inherited from interface \c KeypadListener \c.
+   /// 			In here the key that is being pressed by the KeypadController will be handled.
    void handleMessageKey(char c);
 };
 
